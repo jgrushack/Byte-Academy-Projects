147af62ca1cae189530ec7da83dfde3b30189f54
jgrushack <JGrushack@gmail.com>
2015-04-10 15:42:42 -0400

    added deck + cards class

--- Card-Project/Card.py
+++ Card-Project/Card.py
@@ -1,9 +1,12 @@
-class Card(object):
+class Card():
 	"""Represents a standard playing card """
 	values = ["Ace","2","3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 	suits =["Hearts", "Clubs", "Spades", "Diamonds" ]
 	
-	def __init__(self, suit = 0, value = 2):
+	def __init__(self, suit, value):
 
 		self.suit = suit
 		self.value = value
+
+	def __str__(self):
+		return '%s of %s' % (Card.values[self.value],Card.suits[self.suit])
---
+++ Card-Project/Deck.py
@@ -0,0 +1,55 @@
+import random
+from Card import Card
+
+class Deck():
+	"""docstring for Deck"""
+	def __init__(self):
+		self.cards = []
+		for suit in range(0,4):
+			for value in range(0,13):
+				card = Card(suit, value)
+				self.cards.append(card)
+
+
+	def __str__(self):
+		ret =[]
+		for card in self.cards:
+			print(str(card))
+		return '\n'.join(ret)
+
+
+	def shuffle(self):
+		return random.shuffle(self.cards)
+
+	def drawCard(self):
+		return self.cards.pop()
+
+	def numCards(self):
+		return len(self.cards)
+
+	def addCard(self, card):
+		return self.cards.append(card)
+
+
+
+
+
+deck = Deck()
+print(deck)
+print ("~~~~~~~~~~~~~~")
+print(deck.drawCard())
+print ("~~~~~~~~~~~~~~")
+
+x = deck.drawCard()
+print (deck)
+print ("~~~~~~~~~~~~~~")
+print(str(x))
+print ("~~~~~~~~~~~~~~")
+deck.addCard(x)
+print(deck)
+
+
+
+
+
+
